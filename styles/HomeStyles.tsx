5158e6078440f35f14ebaaf1e1a016805c80aaa0
Pietro Palombini <pietropalo@icloud.com>
2022-07-10 21:53:43 -0300

    Add styles

---
+++ styles/HomeStyles.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components"
+
+const HomeStyles = createGlobalStyle`
+  :root {
+    --accent-light: #4a50fd;
+    --accent-dark: #2e4765;
+
+    --gray-dark: #6c757d;
+    --gray-light: #f4f6fa;
+
+    --border-radius: 18px;
+  }
+
+  h1, h2, h3, h4, h5, h6, p {
+    margin: 0
+  }
+`
+
+export default HomeStyles

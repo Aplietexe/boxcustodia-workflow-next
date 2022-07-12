a0f1a32f12027e5051d84a7d8fc597aac6ec0ee2
Pietro Palombini <pietropalo@icloud.com>
2022-07-12 00:28:58 -0300

    Adapt Sidebar styles for Next

--- components/Home/Sidebar/index.tsx
+++ components/Home/Sidebar/index.tsx
@@ -1,45 +1,9 @@
 import { useCallback } from "react"
 import { stack as Menu } from "react-burger-menu"
-import styled from "styled-components"
 
 import type { SetState } from "lib/types"
 
-import "./defaultStyles.css"
-
-const Styles = styled.div`
-  .bm-burger-button {
-    display: none;
-  }
-
-  .bm-menu {
-    overflow: hidden;
-    background-color: var(--accent-dark);
-  }
-
-  .bm-cross {
-    background-color: var(--gray-light);
-  }
-
-  .bm-item {
-    font-family: "Poppins", sans-serif;
-    font-size: 1.5rem;
-    font-weight: 500;
-    color: var(--text-color, var(--gray-light));
-
-    text-decoration: none;
-    margin-block: 0.75rem;
-    padding: 0.25rem;
-
-    &:hover {
-      color: var(--accent-light);
-    }
-  }
-`
-
-const InvertedLink = styled.a`
-  background-color: var(--gray-light);
-  --text-color: var(--accent-dark);
-`
+import Styles from "./styles"
 
 interface SidebarProps {
   isSidebarOpen: boolean
@@ -63,9 +27,9 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) => {
         <a href="#company" onClick={handleClose}>
           Nosotros
         </a>
-        <InvertedLink href="#contact-form" onClick={handleClose}>
+        <a className="inverted" href="#contact-form" onClick={handleClose}>
           Contacto
-        </InvertedLink>
+        </a>
       </Menu>
     </Styles>
   )

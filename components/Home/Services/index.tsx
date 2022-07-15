b500436c0de1f087e68fc703c2e36ceaaa411c92
Pietro Palombini <pietropalo@icloud.com>
2022-07-15 02:23:26 -0300

    Adapt Services side image to next/image

--- components/Home/Services/index.tsx
+++ components/Home/Services/index.tsx
@@ -1,45 +1,64 @@
 import Image from "next/image"
+import { useContext } from "react"
 import { Col, Row } from "react-bootstrap"
-import styled from "styled-components"
 
 import StyledH2 from "components/StyledH2"
 import StyledP from "components/StyledP"
+import { SidePadding } from "lib/context/Home"
+import { useBreakpoints } from "lib/hooks"
 import sideImage from "public/Home/Services/side.webp"
 
 import content from "./content"
 import Service from "./Service"
 
-const ImageWrapper = styled.div`
-  transform: translateX(13.5%);
-`
+const Services = () => {
+  const breakpoints = useBreakpoints()
+  const sidePadding = useContext(SidePadding)
 
-const Services = () => (
-  <>
-    <Row className="pt-5" id="services">
-      <Col className="pt-5 mb-4">
-        <StyledH2 className="mb-3">
-          Soluciones a medida de nuestros clientes
-        </StyledH2>
-        <StyledP>
-          Elegí los microservicios que mejor se adapten a tu proceso de negocio.
-        </StyledP>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} xxl={7}>
-        <Row>
-          {content.map((service) => (
-            <Service {...service} key={service.text} />
-          ))}
-        </Row>
-      </Col>
-      <Col className="d-none d-xxl-flex align-items-center justify-content-end">
-        <ImageWrapper>
-          <Image alt="Personas" height={746} src={sideImage} width={748} />
-        </ImageWrapper>
-      </Col>
-    </Row>
-  </>
-)
+  return (
+    <>
+      <Row className="pt-5" id="services">
+        <Col className="pt-5 mb-4">
+          <StyledH2 className="mb-3">
+            Soluciones a medida de nuestros clientes
+          </StyledH2>
+          <StyledP>
+            Elegí los microservicios que mejor se adapten a tu proceso de
+            negocio.
+          </StyledP>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} xxl={7}>
+          <Row>
+            {content.map((service) => (
+              <Service {...service} key={service.text} />
+            ))}
+          </Row>
+        </Col>
+        {breakpoints.xxl && (
+          <Col className="d-flex align-items-center">
+            <div
+              css={`
+                width: 100%;
+
+                & > span {
+                  transform: translateX(min(13.5%, ${sidePadding}*0.75));
+                }
+              `}
+            >
+              <Image
+                alt="Personas"
+                layout="responsive"
+                sizes={`calc((100vw - ${sidePadding} * 2) * (5/12))`}
+                src={sideImage}
+              />
+            </div>
+          </Col>
+        )}
+      </Row>
+    </>
+  )
+}
 
 export default Services

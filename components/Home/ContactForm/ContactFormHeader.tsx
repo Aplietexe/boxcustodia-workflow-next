8851e265cfa96be205fa27f2c24586f79d70bd81
Pietro Palombini <pietropalo@icloud.com>
2022-07-14 23:32:43 -0300

    Fix ContactFormHeader rendering double Row

--- components/Home/ContactForm/ContactFormHeader.tsx
+++ components/Home/ContactForm/ContactFormHeader.tsx
@@ -9,17 +9,19 @@ import backgroundImage from "public/Home/ContactForm/background.webp"
 import header from "public/Home/ContactForm/header.png"
 
 const ContactFormHeader = () => (
-  <Row className="py-5 position-relative" id="contact-form">
-    <BackgroundImage src={backgroundImage} />
-    <Pad>
-      <Col className="text-center py-4" xs={12}>
-        <Image alt="Retratos" height={187} src={header} width={359} />
-      </Col>
-      <Col className="text-center py-4" xs={12}>
-        <StyledH2 className="mb-4">¿Cómo podemos ayudarlo?</StyledH2>
-        <StyledP>Llene el formulario</StyledP>
-      </Col>
-    </Pad>
+  <Row className="py-5" id="contact-form">
+    <Col className="position-relative">
+      <BackgroundImage src={backgroundImage} />
+      <Pad>
+        <Col className="text-center py-4" xs={12}>
+          <Image alt="Retratos" height={187} src={header} width={359} />
+        </Col>
+        <Col className="text-center py-4" xs={12}>
+          <StyledH2 className="mb-4">¿Cómo podemos ayudarlo?</StyledH2>
+          <StyledP>Llene el formulario</StyledP>
+        </Col>
+      </Pad>
+    </Col>
   </Row>
 )
 

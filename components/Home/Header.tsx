58571d129df19ff2253bc8831bfd76292fef35c7
Pietro Palombini <pietropalo@icloud.com>
2022-07-15 22:20:11 -0300

    Fix CLS in Header - Rendering depending on the viewport size caused LS on JS load

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -4,7 +4,6 @@ import { Col, Row } from "react-bootstrap"
 import styled from "styled-components"
 
 import AccentButton from "components/AccentButton"
-import Pad from "components/Pad"
 import StyledH2 from "components/StyledH2"
 import { SidePadding } from "lib/context/Home"
 import { useBreakpoints } from "lib/hooks"
@@ -20,64 +19,61 @@ const Paragraph = styled.p`
 const Header = () => {
   const sidePadding = useContext(SidePadding)
 
-  const imageCol = (
-    <Col xl={{ order: 2, span: 6 }} xs={12}>
-      <Image
-        alt="Imagen del encabezado"
-        layout="responsive"
-        priority
-        sizes={`(max-width:1199px) calc(100vw - ${sidePadding} * 2 - 1.5rem),
-                calc(50vw - ${sidePadding} - 0.75rem)`}
-        src={image}
-      />
-    </Col>
-  )
+  const { breakpointSizes } = useBreakpoints
 
-  const textCol = (
-    <Col
-      className="align-items-xl-center d-xl-flex"
-      xl={{ order: 1, span: 6 }}
-      xs={12}
+  return (
+    <Row
+      className="my-4 pt-xl-4"
+      css={`
+        @media (min-width: ${breakpointSizes.lg}px) {
+          padding-inline: ${sidePadding};
+        }
+      `}
     >
-      <Row>
-        <Col xs={12}>
-          <StyledH2 className="pb-3">
-            Modernizar tu negocio puede ser fácil
-          </StyledH2>
-        </Col>
-        <Col className="mb-4" xs={12}>
-          <Paragraph>
-            A través de esta herramienta podrás aumentar la productividad de tu
-            negocio eliminando la gestión manual de documentos, tareas o
-            información que va pasando de persona a persona.
-          </Paragraph>
-        </Col>
-        <Col xs={6}>
-          <AccentButton href="#contact-form">Solicitar cotización</AccentButton>
-        </Col>
-      </Row>
-    </Col>
-  )
+      <Col xl={{ order: 2, span: 6 }} xs={12}>
+        <Image
+          alt="Imagen del encabezado"
+          layout="responsive"
+          priority
+          sizes={`(max-width:1199px) calc(100vw - ${sidePadding} * 2 - 1.5rem),
+                calc(50vw - ${sidePadding} - 0.75rem)`}
+          src={image}
+        />
+      </Col>
+      <Col
+        className="align-items-xl-center d-xl-flex"
+        css={`
+          padding-inline: ${sidePadding};
 
-  const large = (
-    <Pad>
-      <Row className="my-4 pt-xl-4">
-        {imageCol}
-        {textCol}
-      </Row>
-    </Pad>
-  )
-
-  const small = (
-    <Row className="my-4 pt-xl-4">
-      {imageCol}
-      <Pad>{textCol}</Pad>
+          @media (min-width: ${breakpointSizes.lg}px) {
+            padding-inline: 0;
+          }
+        `}
+        xl={{ order: 1, span: 6 }}
+        xs={12}
+      >
+        <Row>
+          <Col xs={12}>
+            <StyledH2 className="pb-3">
+              Modernizar tu negocio puede ser fácil
+            </StyledH2>
+          </Col>
+          <Col className="mb-4" xs={12}>
+            <Paragraph>
+              A través de esta herramienta podrás aumentar la productividad de
+              tu negocio eliminando la gestión manual de documentos, tareas o
+              información que va pasando de persona a persona.
+            </Paragraph>
+          </Col>
+          <Col xs={6}>
+            <AccentButton href="#contact-form">
+              Solicitar cotización
+            </AccentButton>
+          </Col>
+        </Row>
+      </Col>
     </Row>
   )
-
-  const breakpoints = useBreakpoints()
-
-  return breakpoints.lg ? large : small
 }
 
 export default Header

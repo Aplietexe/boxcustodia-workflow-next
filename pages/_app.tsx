e43549288c26768b85cf3265366eba86bf5d4575
Pietro Palombini <pietropalo@icloud.com>
2022-07-15 21:10:10 -0300

    Add viewport meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "bootstrap/dist/css/bootstrap.css"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 
 import GlobalStyle from "styles/GlobalStyle"
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </Head>
     <GlobalStyle />
     <Component {...pageProps} />
   </>

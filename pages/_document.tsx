e6900193cb1deffe4f2f097e5cae181663d9dffe
Pietro Palombini <pietropalo@icloud.com>
2022-07-16 00:02:03 -0300

    Add html tag lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="es">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;800&family=Poppins:wght@300;400;500;600&family=Raleway&display=swap"
